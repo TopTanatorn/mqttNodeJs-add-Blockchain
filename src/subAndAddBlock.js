f8ee3eb0893e5888afbd1d6e6e9a4ce44d6b9394
TopTanatorn <s6230613017@phuket.psu.ac.th>
2022-01-25 03:42:54 +0700

    add blockchain on mqtt

--- src/subAndAddBlock.js
+++ src/subAndAddBlock.js
@@ -1,11 +1,12 @@
 const mqtt = require("mqtt");
-
+const {Blockchain, Block} = require('./blockchain');
 const MQTT_SERVER = "192.168.0.164";
 const MQTT_PORT = "1883";
 //if your server don't have username and password let blank.
 const MQTT_USER = ""; 
 const MQTT_PASSWORD = "";
-
+let iotChain = new Blockchain();
+iotChain.createGenesisBlock();
 // Connect MQTT
 var client = mqtt.connect({
     host: MQTT_SERVER,
@@ -27,5 +28,6 @@ client.on('connect', function () {
 // Receive Message and print on terminal
 client.on('message', function (topic, message) {
     // message is Buffer
+    iotChain.addBlock(new Block(Date.now(),message));
     console.log(message.toString());
 });
